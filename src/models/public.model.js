ffba739056660e0e96e3f490f7a6b7675299fc0c
Diana Taborda Delgado <dianafer_taborda@hotmail.com>
2023-11-30 22:55:49 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/models/public.model.js
+++ src/models/public.model.js
@@ -1,9 +1,17 @@
-const getByRating = () => {
-  return db.query('SELECT * FROM tfm_db.users');
+
+const getByHighestrated = async () => {
+
+  return db.query("SELECT  avg(h.puntuacion),h.logopeda_id FROM logopedas.usuarios u, logopedas_has_clientes H WHERE h.logopeda_id = u.id AND  u.rol = 'logopeda' and u.status = 1 GROUP BY h.logopeda_id having avg(h.puntuacion) > 3 order by h.logopeda_id desc limit 20");
+};
+
+const getByRating = async () => {
+  //return db.query("SELECT  u.id,u.nombre,u.apellidos,u.email ,l.telefono,u.localidad,  l.precio, l.descripcion, l.experiencia,u.imagen ,h.puntuacion FROM  logopedas.usuarios u, logopedas.logopeda_datos l, logopedas_has_clientes h WHERE u.id = l.usuario_id AND h.logopeda_id = u.id AND  u.rol = 'logopeda' and u.status = 1 AND h.puntuacion in (4, 5)order by h.puntuacion desc");
+  return db.query("SELECT  u.id,u.nombre,u.apellidos,u.email ,l.telefono,u.localidad,  l.precio, l.descripcion, l.experiencia,u.imagen ,h.puntuacion FROM  logopedas.usuarios u, logopedas.logopeda_datos l, logopedas_has_clientes h WHERE u.id = l.usuario_id AND h.logopeda_id = u.id AND  u.rol = 'logopeda' and u.status = 1 AND h.puntuacion in (4, 5)order by h.puntuacion desc");
 };
 
 const getByMap = () => {
   return db.query('SELECT * FROM tfm_db.users');
 };
 
-module.exports = { getByRating, getByMap };
+
+module.exports = { getByRating, getByHighestrated };
--- src/routes/api/usuarios/logopedas.js
+++ src/routes/api/usuarios/logopedas.js
@@ -6,10 +6,7 @@ router.get('/', LogopedasController.getAllLogopedas); // (getAll. si es admin, m
 router.get('/:id', LogopedasController.getLogopedaById); // (versión con los campos necesarios para mostrar a clientes/admins)
 router.get('/infancia', LogopedasController.getInfancia); // (si infancia_o_adulto es infancia o ambos)
 router.get('/adultos', LogopedasController.getAdultos); // (idem pero con adultos o ambos)
-router.get(
-  '/especialidad/:idespecialidad',
-  LogopedasController.getByEspecialidadId
-); // (todos los que tienen relación con id especialidad en especialidades_has_logopedas)
+router.get('/especialidad/:idespecialidad', LogopedasController.getByEspecialidad); // (todos los que tienen relación con id especialidad en especialidades_has_logopedas)
 router.post('/conectar/:idlogopeda', LogopedasController.postConectarLogopeda); // (crea relación del id del token con logopeda, si es el cliente)
 router.put('/conectar/:idcliente', LogopedasController.putConectarIdCliente); // (cambia status del id del token con el cliente, si es el logopeda)
 
