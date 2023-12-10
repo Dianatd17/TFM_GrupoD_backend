99569c968ff8ec0fbf77b1867ec5a9a9322f251d
Christel González Canales <kirisame@gmail.com>
2023-12-10 02:16:38 +0100

    Desarrolladas rutas para /api/usuarios/admins

--- src/middlewares/admins.middleware.js
+++ src/middlewares/admins.middleware.js
@@ -2,9 +2,10 @@ const UsuarioModel = require('../models/usuario.model');
 
 const checkAdmin = async (req, res, next) => {
   // Verificar si el usuario tiene el rol admin
-  const { userId } = req.params;
+  const [[user]] = req.user;
+  const { id } = user;
   try {
-    const usuario = await UsuarioModel.selectUsuarioById(userId);
+    const [[usuario]] = await UsuarioModel.selectUsuarioById(id);
     if (!usuario || usuario.rol !== 'admin') {
       return res.json({
         fatal: 'El usuario no tiene asignado el rol de administrador',
