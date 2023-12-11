69fd3c9cb6aa8f16df2bedabc9002bdc1ca28250
bielmc11 <bielmarticr@gmail.com>
2023-12-11 13:32:35 +0100

    Modificacion rutas para valorar

--- src/models/cliente.model.js
+++ src/models/cliente.model.js
@@ -8,7 +8,7 @@ const selectClienteById = (clienteId) => {
 
 const selectLogopedaByClientes = (idCliente) => {
   return db.query(
-    `SELECT u.nombre, l.logopeda_id , u.apellidos, u.email, u.localidad, d.precio, d.experiencia, l.puntuacion, d.infancia_o_adulto,d.descripcion, u.imagen, u.rol, u.status as estado_u, l.fecha_inicio, l.status
+    `SELECT l.id, u.nombre, l.logopeda_id , l.cliente_id, u.apellidos, u.email, u.localidad, d.precio, d.experiencia, l.puntuacion, d.infancia_o_adulto,d.descripcion, u.imagen, u.rol, u.status as estado_u, l.fecha_inicio, l.status
     FROM logopedas.logopedas_has_clientes l, logopedas.usuarios u, logopedas.logopeda_datos d
     WHERE l.logopeda_id = u.id AND l.cliente_id = ? AND l.logopeda_id = d.usuario_id;`,
     [idCliente]
