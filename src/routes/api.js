94922002205e759bcac7aeaf5cea98cda3422f7f
Christel González Canales <kirisame@gmail.com>
2023-12-11 13:55:39 +0100

    Eliminados ficheros y código extra

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,6 +5,5 @@ router.use('/public', require('./api/public'));
 router.use('/enviaremail', require('./api/enviarEmail'));
 router.use('/usuarios', require('./api/usuarios'));
 router.use('/especialidades', require('./api/especialidades'));
-router.use('/notificaciones', require('./api/notificaciones'));
 
 module.exports = router;
--- src/routes/api/public.js
+++ src/routes/api/public.js
@@ -3,6 +3,5 @@ const router = require('express').Router();
 const PublicController = require('../../controllers/public.controller');
 
 router.get('/', PublicController.getByRating);
-//router.get('/', PublicController.getByLocation);
 
 module.exports = router;
